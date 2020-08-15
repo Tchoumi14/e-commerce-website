7f3f7448c8ec9e658112cc850c13671b9637902e
Brice <brice@zeitform.de>
2020-08-16 01:53:45 +0200

    email and password auth using firebase

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,40 +1,55 @@
-import React from 'react'
+import React from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
-
+import { auth } from './../../firebase/utils';
 import Logo from './../../assets/logo.png';
 
-  const Header = props => {
- /* const dispatch = useDispatch();
-  const { currentUser } = useSelector(mapState);
-
-  const signOut = () => {
-    dispatch(signOutUserStart()); 
-  };*/
-
+const Header = props => {
+	 const { currentUser } = props;
+	 
   return (
     <header className="header">
       <div className="wrap">
         <div className="logo">
-         
-           <Link to="/">
-            <img src={Logo} alt="BriceTchoumi LOGO" />
+          <Link to="/">
+            <img src={Logo} alt="SimpleTut LOGO" />
           </Link>
-           </div>
-		   
-	 <div className="callToActions">
-          <ul>
-            <li>
-              <Link to="/registration">
-                Register
-              </Link>
-            </li>
-          </ul>
         </div>
+        <div className="callToActions">
+		
+           {currentUser && (
+            <ul>
+              <li>
+                <span onClick={() => auth.signOut()}>
+                  LogOut
+                </span>
+              </li>
+            </ul>
+          )}
 
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to="/registration">
+                  Register
+              </Link>
+           </li>
+              <li>
+                <Link to="/login">
+                  Login
+              </Link>
+				</li>
+            </ul>
+          )}
+		  
+        </div>
       </div>
     </header>
   );
-  };
+};
+
+Header.defaultProps = {
+  currentUser: null
+};
 
 export default Header;
---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDNmz1O99-PYPbeUMxG9-f_yo_pQY5lieQ",
+    authDomain: "ecommerce-website-9f251.firebaseapp.com",
+    databaseURL: "https://ecommerce-website-9f251.firebaseio.com",
+    projectId: "ecommerce-website-9f251",
+    storageBucket: "ecommerce-website-9f251.appspot.com",
+    messagingSenderId: "636739334581",
+    appId: "1:636739334581:web:09395d1081f7761b66a9fc"
+  };
+
