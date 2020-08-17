123bc66fb7eb1518cb13bb3df2216649b3bf3813
Brice <brice@zeitform.de>
2020-08-17 02:45:09 +0200

    email and password reset

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { auth, handleUserProfile } from './firebase/utils';
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
+import Recovery from './pages/Recovery';
 import './default.scss';
 
 //layouts
@@ -73,6 +74,11 @@ class App extends Component {
             render={() => currentUser ? <Redirect to="/" /> : (
               <MainLayout currentUser={currentUser}>
                 <Login />
+              </MainLayout>
+            )} />
+			<Route path="/recovery" render={() => (
+              <MainLayout>
+                <Recovery />
               </MainLayout>
             )} />
         </Switch>
